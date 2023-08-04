9e3e2ca9e43b42f8edaf6671f83f7417a1a30c6a
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-04 17:30:27 +0100

    Update Checkout.js

--- Checkout.js
+++ Checkout.js
@@ -1,12 +1,27 @@
-//Checkout.js
+// Checkout.js
 
-import React from 'react';
+import React from "react";
 
-const Checkout = () => {
+const Checkout = ({ cartItems, totalPrice }) => {
+  if (!cartItems || cartItems.length === 0) {
+    // When cartItems is not available or is empty, show the "Cart is empty or state data is missing" message
+    return <div>Cart is empty or state data is missing.</div>;
+  }
+
   return (
-    <div>
+    <div className='checkout-container'>
       <h2>Checkout</h2>
-      {/* Your checkout form and other components can go here */}
+      <div>
+        {cartItems.map((item) => (
+          <div className='checkout-item' key={item.id}>
+            <p>
+              {item.name} - ${item.price} x {item.quantity}
+            </p>
+          </div>
+        ))}
+        <p className='checkout-total'>Total Price: ${totalPrice}</p>
+        {/* Your checkout form and other components can go here */}
+      </div>
     </div>
   );
 };
