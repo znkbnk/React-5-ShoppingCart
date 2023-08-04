e9c5ef425a66e586753e3d321183f53cee858b1f
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-04 16:44:05 +0100

    Create Checkout.js

---
+++ Checkout.js
@@ -0,0 +1,14 @@
+//Checkout.js
+
+import React from 'react';
+
+const Checkout = () => {
+  return (
+    <div>
+      <h2>Checkout</h2>
+      {/* Your checkout form and other components can go here */}
+    </div>
+  );
+};
+
+export default Checkout;
