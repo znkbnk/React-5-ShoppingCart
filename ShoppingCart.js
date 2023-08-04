7f25d82b99196785a604cb422054529402d06e18
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-04 17:31:06 +0100

    Update ShoppingCart.js

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,40 +1,15 @@
-//ShoppingCart.js
+// ShoppingCart.js
 
-import React, { useState } from 'react';
-import Item from './Item';
+import React from "react";
+import { Link } from "react-router-dom";
+import Item from "./Item";
 
-const ShoppingCart = () => {
-  const [cartItems, setCartItems] = useState([
-    { id: 1, name: 'Item 1', price: 10, quantity: 1 },
-    { id: 2, name: 'Item 2', price: 20, quantity: 2 },
-    // Add more items as needed
-  ]);
-
-  const handleRemoveItem = (itemToRemove) => {
-    const updatedItems = cartItems.filter((item) => item.id !== itemToRemove.id);
-    setCartItems(updatedItems);
-  };
-
-  const handleIncrementItem = (itemToIncrement) => {
-    const updatedItems = cartItems.map((item) =>
-      item.id === itemToIncrement.id
-        ? { ...item, quantity: item.quantity + 1 }
-        : item
-    );
-    setCartItems(updatedItems);
-  };
-
-  const handleDecrementItem = (itemToDecrement) => {
-    const updatedItems = cartItems.map((item) =>
-      item.id === itemToDecrement.id
-        ? { ...item, quantity: item.quantity - 1 }
-        : item
-    );
-    setCartItems(updatedItems);
-  };
-
+const ShoppingCart = ({ cartItems, onRemove, onIncrement, onDecrement }) => {
   const calculateTotalPrice = () => {
-    return cartItems.reduce((total, item) => total + item.price * item.quantity, 0);
+    return cartItems.reduce(
+      (total, item) => total + item.price * item.quantity,
+      0
+    );
   };
 
   return (
@@ -43,13 +18,20 @@ const ShoppingCart = () => {
         <Item
           key={item.id}
           item={item}
-          onRemove={handleRemoveItem}
-          onIncrement={handleIncrementItem}
-          onDecrement={handleDecrementItem}
+          onRemove={() => onRemove(item.id)}
+          onIncrement={() => onIncrement(item.id)}
+          onDecrement={() => onDecrement(item.id)}
         />
       ))}
-      <p>Total Price: ${calculateTotalPrice()}</p>
-      <button>Checkout</button>
+      <p className='total-price'>Total Price: ${calculateTotalPrice()}</p>
+      <Link
+        to={{
+          pathname: "/checkout",
+          state: { cartItems, totalPrice: calculateTotalPrice() },
+        }}
+      >
+        <button className='checkout-button'>Checkout</button>
+      </Link>
     </div>
   );
 };
