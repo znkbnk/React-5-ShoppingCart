93d4f2aebd0012268793c94f62f11382c68b0a7d
znkbnk <133157126+znkbnk@users.noreply.github.com>
2023-08-04 17:30:49 +0100

    Update Item.js

--- Item.js
+++ Item.js
@@ -1,16 +1,16 @@
-//Item.js
+// Item.js
 
-import React from 'react';
+import React from "react";
 
 const Item = ({ item, onRemove, onIncrement, onDecrement }) => {
   return (
-    <div>
+    <div className='item'>
       <h3>{item.name}</h3>
       <p>Price: ${item.price}</p>
       <p>Quantity: {item.quantity}</p>
-      <button onClick={() => onIncrement(item)}>Increment</button>
-      <button onClick={() => onDecrement(item)}>Decrement</button>
-      <button onClick={() => onRemove(item)}>Remove</button>
+      <button onClick={onIncrement}>+</button>
+      <button onClick={onDecrement}>-</button>
+      <button onClick={onRemove}>Remove</button>
     </div>
   );
 };
